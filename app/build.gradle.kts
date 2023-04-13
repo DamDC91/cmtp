cfd88e813f0895413eec2412c1028faa51554c97
Damien <damiensporting@gmail.com>
2023-04-13 19:55:55 +0200

    jaxb test

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,10 +15,14 @@ javafx {
 
 dependencies {
     implementation("com.google.guava:guava:31.1-jre")
+    implementation("javax.xml.bind:jaxb-api:2.3.1")
+    implementation("javax.activation:javax.activation-api:1.2.0")
+    implementation("org.glassfish.jaxb:jaxb-runtime:2.3.2")
     runtimeOnly("org.openjfx:javafx-graphics:$javafx.version:win")
     runtimeOnly("org.openjfx:javafx-graphics:$javafx.version:linux")
     runtimeOnly("org.openjfx:javafx-graphics:$javafx.version:mac")
 }
+
 
 application {
     mainClass.set("cmtp.HelloWorld")
--- app/src/main/java/cmtp/HelloWorld.java
+++ app/src/main/java/cmtp/HelloWorld.java
@@ -1,5 +1,11 @@
 package cmtp;
  
+import java.io.File;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Unmarshaller;
+
+import generated.Msg;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.Parent;
@@ -11,7 +17,19 @@ public class HelloWorld extends Application
 
     public static void main(String[] args) 
     {
+    	
         launch(args);
+        try {
+	        JAXBContext jc = JAXBContext.newInstance(Msg.class);
+	
+	        Unmarshaller unmarshaller = jc.createUnmarshaller();
+	        File xml = new File("put your xml file path here");
+	        Msg Message = (Msg) unmarshaller.unmarshal(xml);
+	        System.out.println(Message.getHeader().getFrom());
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
     }
 
     @Override
