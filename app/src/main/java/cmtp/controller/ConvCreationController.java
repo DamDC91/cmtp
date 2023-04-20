d2f8af5ff7a82d50a95505d7a818efde88acc492
Damien De Campos <damiensporting@gmail.com>
2023-04-20 19:34:43 +0200

    avoiding empty recipient

--- app/src/main/java/cmtp/controller/ConvCreationController.java
+++ app/src/main/java/cmtp/controller/ConvCreationController.java
@@ -27,33 +27,33 @@ public class ConvCreationController {
 	 @FXML
 	 private void initialize() {
 		 Set<String> plugins = PluginManager.getAllPlugins();
-		 for(String name : plugins)
-		 {
-			 pluginChoiceBox.getItems().add(name);
-		 }
+		 for(String name : plugins)		 
+			 pluginChoiceBox.getItems().add(name);		 
 	 }
 	 
 	 public void onAddRecipientCliked()
 	 {
-		 vbox.getChildren().add(0, new Label(recipientTextField.getText()));
-		 recipientTextField.setText("");
+		 if (!recipientTextField.getText().isBlank())
+		 {
+			 vbox.getChildren().add(0, new Label(recipientTextField.getText()));
+			 recipientTextField.setText("");
+		 }
 	 }
 	 
 	 public void onAddPluginCliked()
 	 {
 		 if(!pluginChoiceBox.getSelectionModel().isEmpty())
 		 {
-			 System.out.println("Adding plugin: "+ pluginChoiceBox.getSelectionModel().getSelectedItem());		
-			 try {
+			System.out.println("Adding plugin: "+ pluginChoiceBox.getSelectionModel().getSelectedItem());		
+			try {
 				 Form f = PluginManager.getPlugin(pluginChoiceBox.getSelectionModel().getSelectedItem()).getDeclaredConstructor().newInstance().getForm();
 				 vbox.getChildren().add(vbox.getChildren().size()-1, new FormView().addForm(f));
 				 System.out.println(f.toString());
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
-			 pluginChoiceBox.getSelectionModel().clearSelection();
-		}
-		
+			pluginChoiceBox.getSelectionModel().clearSelection();
+		 }		
 	 }
 	 
 
